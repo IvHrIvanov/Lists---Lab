7f4a1b03ec5e4b603e57e709ae8e6b224dee698f
IvanHrIvanov <vinko96@abv.bg>
2020-10-16 21:30:07 +0300

    add new Tasks

---
+++ 04. List of Products/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            List<string> producst = new List<string>();
+         
+
+            for (int i = 0; i < rows; i++)
+            {
+                string addProducts = Console.ReadLine();
+
+
+                producst.Add(addProducts);
+
+            }
+            producst.Sort();
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine($"{i+1}.{producst[i]}");
+            }
+        }
+    }
+}
---
+++ Lists - Lab/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._List_Manipulation_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<string> command = Console.ReadLine()
+                .ToUpper()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+            while (command[0].ToUpper()!= "END")
+            {
+                switch (command[0])
+                {
+                    case "ADD":
+                        numbers.Add(int.Parse(command[1]));
+                        break;
+                    case "REMOVE":
+                        numbers.Remove(int.Parse(command[1]));
+                        break;
+                    case "REMOVEAT":
+                        numbers.RemoveAt(int.Parse(command[1]));
+                        break;
+                    case "INSERT":
+                        numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                        break;
+
+                }
+                command = Console.ReadLine()
+                    .ToUpper()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+            }
+            Console.Write(string.Join(" ", numbers));
+        }
+    }
+}
